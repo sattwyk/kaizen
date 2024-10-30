af5e5b8dc7ddb045779c96587b95e136c1d3ccef
sattwyk <hellosattwik@gmail.com>
2024-10-30 15:54:14 +0530

    add db schema and migration for user authentication

--- web/src/db/schema.ts
+++ web/src/db/schema.ts
@@ -1,8 +1,42 @@
-import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
 
-export const usersTable = sqliteTable("users_table", {
-  id: int().primaryKey({ autoIncrement: true }),
+export const user = sqliteTable("user", {
+  id: text().primaryKey(),
   name: text().notNull(),
-  age: int().notNull(),
   email: text().notNull().unique(),
+  emailVerified: integer({ mode: "boolean" }).notNull(),
+  image: text(),
+  createdAt: integer({ mode: "timestamp" }).notNull(),
+  updatedAt: integer({ mode: "timestamp" }).notNull(),
+});
+
+export const session = sqliteTable("session", {
+  id: text().primaryKey(),
+  expiresAt: integer({ mode: "timestamp" }).notNull(),
+  ipAddress: text(),
+  userAgent: text(),
+  userId: text()
+    .notNull()
+    .references(() => user.id),
+});
+
+export const account = sqliteTable("account", {
+  id: text().primaryKey(),
+  accountId: text().notNull(),
+  providerId: text().notNull(),
+  userId: text()
+    .notNull()
+    .references(() => user.id),
+  accessToken: text(),
+  refreshToken: text(),
+  idToken: text(),
+  expiresAt: integer({ mode: "timestamp" }),
+  password: text(),
+});
+
+export const verification = sqliteTable("verification", {
+  id: text().primaryKey(),
+  identifier: text().notNull(),
+  value: text().notNull(),
+  expiresAt: integer({ mode: "timestamp" }).notNull(),
 });
