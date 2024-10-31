c126786e587d68443df16bff8cd222c3a00fbf75
sattwyk <hellosattwik@gmail.com>
2024-10-31 17:14:08 +0530

    feat(db): integrate schema into database connection

--- web/src/db/index.ts
+++ web/src/db/index.ts
@@ -1,9 +1,11 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/libsql";
+import * as schema from "./schema";
 
 export const db = drizzle({
   connection: {
     url: process.env.DATABASE_URL!,
     // authToken: process.env.TURSO_AUTH_TOKEN!
   },
+  schema,
 });
